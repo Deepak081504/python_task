fcfea0444d1c6fe8b2f0a0733ebb355d1693c774
Deepak081504 <Deepakk@thestackly.com>
2026-03-12 12:43:49 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,110 @@
+# # Print Formating
+
+# print("Hello World",end=" ")
+# print("Welcome Python")
+
+# print("Laptop","Mouse","Keyboard",sep="|")
+
+# # Variables
+
+# name="Ravi"
+# age=22
+# city="chennai"
+
+# print(name,age,city,sep="-")
+
+# Multiple Assignment
+
+# name,age,student="Meena",20,True
+
+# print("Name:",name)
+# print("Age:",age)
+# print("Student:",student)
+
+# Indexing
+
+# word="Python"
+
+# print("First Letter:",word[0])
+# print("Third Letter:",word[2])
+# print("Last Letter:",word[-1])
+
+# Arthimetic Operators
+
+# print("25+10=",25+10)
+# print("50-50=",50-50)
+# print("8*5=",8*5)
+# print("100/10=",100/10)
+# print("10%3=",10%3)
+# print("2**4=",2**4)
+# print("20//3=",20//3)
+
+# BODMAS Expression
+
+# print(3+2*5**2)
+
+# Assignment Operator
+
+# num=50
+# num+=25
+
+# print("final value:",num)
+
+# num=100
+# num/=10
+
+# print("result:",num)
+
+# Comparsion Operators
+
+# print("10>5:",10>5)
+# print("20<15:",20<15)
+# print("5==5:",5==5)
+# print("10!=8",10!=8)
+# print("7>=7",7>=7)
+# print("6<=2",6<=2)
+
+# String Comparsion
+
+# a="apple"
+# b="Apple"
+
+# print(a==b)
+
+# Reason: (The Lowercase "a" has a different underlying value (97) than the uppercase "A" (65).Because the very first characters do not match exactly .Python immediately evaluates the statements as False.
+
+# Logical operators
+
+# print(10>5 and 5==5)
+# print(5>10 or 10==10)
+# print(not(5>2))
+
+# Membership Operator
+
+# numbers=[10,20,30,40,50]
+
+# print("20 in numbers:",20 in numbers)
+# print("60 in numbers:",60 in numbers)
+# print("30 not in numbers:",30 not in numbers)
+
+# Swap Variables
+
+# a=10
+# b=20
+
+# a,b=b,a
+# print("a=",a)
+# print("b=",b)
+
+# Bit wise XOR
+
+# a=6
+# b=3
+
+# print("a^b=",a^b)
+
+
+               
+
+
+ 
